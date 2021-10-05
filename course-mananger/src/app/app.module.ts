3e5fe9948749e873efb7b20f341df74a9d90dd35
rpavanello97 <rpavanello97@hotmail.com>
2021-10-04 21:44:40 -0300

    Class finished: Activating routes to access components.

--- course-mananger/src/app/app.module.ts
+++ course-mananger/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ChangeMonetaryValue } from './pipe/changeMonetaryValue.pipe';
 import { ChangeMonetarySimbol } from './pipe/changeMonetarySimbol.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 
 
@@ -23,21 +24,25 @@ import { Error404Component } from './error-404/error-404.component';
     ChangeMonetaryValue,
     ChangeMonetarySimbol,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full' //Raiz route
-      },
-      {
         path: 'courses', component: CourseListComponent
       },
       {
+        path: 'courses/info/:id', component: CourseInfoComponent //Route with parameter
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full' //Raiz route
+      },
+      {
         path: '**', component: Error404Component //When angular do not find any destiny
-      }
+      }         
     ])
   ],
   providers: [],
