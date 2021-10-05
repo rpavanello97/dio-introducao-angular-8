cb7a93219718177c0acddb4a99086f146f633317
rpavanello97 <rpavanello97@hotmail.com>
2021-10-04 21:07:55 -0300

    Class Finished: Protecting routes with guards

--- course-mananger/src/app/app.module.ts
+++ course-mananger/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
@@ -8,6 +9,10 @@ import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { ChangeMonetaryValue } from './pipe/changeMonetaryValue.pipe';
 import { ChangeMonetarySimbol } from './pipe/changeMonetarySimbol.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,11 +21,24 @@ import { ChangeMonetarySimbol } from './pipe/changeMonetarySimbol.pipe';
     StarComponent,
     ReplacePipe,
     ChangeMonetaryValue,
-    ChangeMonetarySimbol
+    ChangeMonetarySimbol,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full' //Raiz route
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component //When angular do not find any destiny
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
