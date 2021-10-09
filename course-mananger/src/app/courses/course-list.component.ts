ac47618a07bd71db997ac53a7aeb9e4671c7e1eb
rpavanello97 <rpavanello97@hotmail.com>
2021-10-09 15:08:59 -0300

    new "delete" course function created using httpClient.

--- course-mananger/src/app/courses/course-list.component.ts
+++ course-mananger/src/app/courses/course-list.component.ts
@@ -3,11 +3,13 @@ import { Course } from "./course";
 import { CourseService } from "./course.service";
 
 @Component({
-    templateUrl: './course-list.component.html' 
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.css']
+
 })
 
 export class CourseListComponent implements OnInit {
-    
+
     filteredCourses: Course[] = [];
     _courses: Course[] = [];
     _filterBy: string;
@@ -24,14 +26,24 @@ export class CourseListComponent implements OnInit {
                 this._courses = courses;
                 this.filteredCourses = this._courses;
             },
-            error: err => console.log('Error'+err)
+            error: err => console.log('Error' + err)
+        })
+    }
+
+    delete(id: Number): void {
+        this.courseService.deleteById(id).subscribe({
+            next: () => {
+                console.log('Success deleted!')
+                this.retrieveAll();
+            },
+            error: err => console.log('Error' + err)
         })
     }
 
     set filter(value: string) {
         this._filterBy = value;
         this.filteredCourses = this._courses.filter((course: Course) => {
-            return course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1 
+            return course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1
         })
     }
 
