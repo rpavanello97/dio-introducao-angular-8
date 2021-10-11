e8b032f69203c7f5eceae1c653dbf8e101642f5d
rpavanello97 <rpavanello97@hotmail.com>
2021-10-11 20:08:48 -0300

    "error" component inside the "core"

--- course-mananger/src/app/app.module.ts
+++ course-mananger/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course-module';
 import { NavBarModule } from './core/component/nav-bar.module';
 
@@ -13,22 +12,20 @@ import { NavBarModule } from './core/component/nav-bar.module';
 
 @NgModule({
   declarations: [
-    AppComponent,       
-    Error404Component
+    AppComponent
+
   ],
-  imports: [ 
-    BrowserModule, 
-    CourseModule,  
+  imports: [
+    BrowserModule,
+    CourseModule,
     FormsModule,
     HttpClientModule,
     NavBarModule,
-    RouterModule.forRoot([      
+    RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full' //Raiz route
-      },
-      {
-        path: '**', component: Error404Component //When angular do not find any destiny
-      }         
+      }
+
     ])
   ],
   providers: [],
--- course-mananger/src/app/core/component/nav-bar.module.ts
+++ course-mananger/src/app/core/component/nav-bar.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { Error404Component } from "./error-404/error-404.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @NgModule({
@@ -7,7 +8,11 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
         NavBarComponent
     ],
     imports: [
-        RouterModule
+        RouterModule.forChild([
+            {
+                path: '**', component: Error404Component //When angular do not find any destiny
+            }
+        ])
     ],
     exports: [
         NavBarComponent
